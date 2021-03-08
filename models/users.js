36af5a1af0f5e77d7810cf9e2508f99fb5e46d89
unknown <san.smart09@rediffmail.com>
2021-03-08 18:28:49 +0530

    update package

--- models/users.js
+++ models/users.js
@@ -1,19 +1,18 @@
-// const mongoose = require('mongoose');
-// const userSchema = new mongoose.Schema({
-//     name : {
-//         type : String,
-//         required : true
-//     },
-//     email : {
-//         type : String,
-//         required : true,
-//         unique : true
-//     },
-//     gender : String,
-//     status : String
+const mongoose = require('mongoose');
+const userSchema = new mongoose.Schema({
+    name : {
+        type : String,
+        required : true
+    },
+    email : {
+        type : String,
+        required : true,
+        unique : true
+    },
+    gender : String
     
-// })
+})
 
-// const User = mongoose.model('userdb', userSchema)
+const User = mongoose.model('user', userSchema)
 
-// module.exports = User
+module.exports = User
--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 
-// const User = require('')
+const User = require('../models/users')
 const express = require('express')
+const { response } = require('express')
 
 const userRoute = express.Router()
 
@@ -8,8 +9,13 @@ userRoute.get('/', (req, res)=>{
     res.render('home')
 })
 
-userRoute.post('/', (req, res)=>{
-    res.send(req.body)
+userRoute.post('/', async (req, res)=>{
+    const user = await new User(req.body)
+    user.save().then((user)=>{
+        res.status(200).send(user)
+    }).catch((err)=>{
+        res.status(400).send('You got this error'+err)
+    })
 })
 
 module.exports = userRoute
